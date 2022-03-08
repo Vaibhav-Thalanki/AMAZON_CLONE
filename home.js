077e2ff68579b9c778c3dcdd73f9b7987e4e864d
Puffyfish <93864592+Puffyfish@users.noreply.github.com>
2022-03-08 10:08:05 +0100

    horizontal scroll update
    the JS works now but for only one DIV.
    have to find out how to set JS with arrays.

--- home.js
+++ home.js
@@ -1,29 +1,29 @@
-const slider = document.querySelector('.image-container');
-let isDown = false;
-let startX;
-let scrollLeft;
+  var slider = document.querySelector("#imageContainer");
+  let isDown = false;
+  let startX;
+  let scrollLeft;
 
-slider.addEventListener('mousedown', (e) => {
-  isDown = true;
-  slider.classList.add('active');
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-});
+  slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+  });
 
-slider.addEventListener('mouseleave', () => {
-  isDown = false;
-  slider.classList.remove('active');
-});
+  slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('active');
+  });
 
-slider.addEventListener('mouseup', () => {
-  isDown = false;
-  slider.classList.remove('active');
-});
+  slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('active');
+  });
 
-slider.addEventListener('mousemove', (e) => {
-  if (!isDown) return;  // stop the fn from running
-  e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX) * 3;
-  slider.scrollLeft = scrollLeft - walk;
-});
+  slider.addEventListener('mousemove', (e) => {
+    if (!isDown) return;  // stop the fn from running
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 3;
+    slider.scrollLeft = scrollLeft - walk;
+  });
